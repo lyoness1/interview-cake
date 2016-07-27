941e2f4290fb2cd7b386727bed78d7b35c275b12
lyoness1 <alyoness1@gmail.com>
2016-07-27 16:00:17 -0700

    merge lists

---
+++ 43-girlscout-cookies.py
@@ -0,0 +1,13 @@
+"""
+Print merged lists in sorted order. Inputs will already be sorted. 
+
+"""
+
+def merge_lists(list1, list2):
+    """Merges lists in order
+
+        >>> merge_lists([3, 4, 6, 10, 11, 15], [1, 5, 8, 12, 14, 19])
+        [1, 3, 4, 5, 6, 8, 10, 11, 12, 14, 15, 19]
+
+    """
+
